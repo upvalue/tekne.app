280290d501b411f9741622fe05c6ccb9d397ba02
Phil <phil@upvalue.io>
2025-08-24 18:20:59 -0700

    update site; add beginning of docs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
+  adapter: vercel(),
 });
